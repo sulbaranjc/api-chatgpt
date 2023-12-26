10ccd1219f39bface86bfa1c3f05795845c427b6
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-12-26 13:35:22 +0100

    PRIMER OK

---
+++ src/test/java/com/crud/chatGPT/ChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.chatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
